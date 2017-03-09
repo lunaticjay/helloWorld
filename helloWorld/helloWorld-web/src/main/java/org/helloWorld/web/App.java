de0940716a75d828c25aa4d8135a3236212dcabd
LunaticJay <lunaticjay@163.com>
2017-03-09 21:54:12 +0800

    新建项目

---
+++ helloWorld/helloWorld-web/src/main/java/org/helloWorld/web/App.java
@@ -0,0 +1,13 @@
+package org.helloWorld.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
